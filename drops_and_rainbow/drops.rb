ccc10d5992bee28341efe9585c00ae3b3a2d264c
Gloritz <gloriaahlers@posteo.de>
2019-10-01 19:50:41 +0200

    Give the programm an explanation, ask for user input and a third option by using elsif

--- drops_and_rainbow/drops.rb
+++ drops_and_rainbow/drops.rb
@@ -1,9 +1,16 @@
-puts "1 drop"
-a = [*2..100]
-a.each do |number|
-  if number % 10 == 0
-    puts "rainbow"
-  else
-    puts "#{number} drops"
-  end
-end
+puts "This programm will create a lot of drops and some rainbows."
+puts "Do you want to start?"
+puts "Press Yes or No"
+  user_input=gets.chomp
+    if user_input == "Yes"
+      a = [*1..100]
+      a.each do |number|
+        if number == 1
+          puts "#{number} drop"
+        elsif number % 10 == 0
+          puts "rainbow"
+        else
+          puts "#{number} drops"
+        end
+      end
+    end
